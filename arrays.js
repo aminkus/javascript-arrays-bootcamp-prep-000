9638f975311551d5fbfeb29424026d8459fda93e
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-15 04:44:06 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray(array, element){
+  return ['foo',...array]
+}
+function destructivelyAddElementToBeginningOfArray(array,element){array.unshift(element)
+    return array}
+function addElementToEndOfArray(array, element) {
+  return [...array, 'foo']}
+function destructivelyAddElementToEndOfArray(array,element){array.push('foo')
+    return array}
+function accessElementInArray(array, index){return array[2]}
+function destructivelyRemoveElementFromBeginningOfArray(array){return array.shift()}
+function removeElementFromBeginningOfArray(array){ return array.slice(1)}
+
+
+
+
+    
+  
+
+    
+
+
+
+
+
