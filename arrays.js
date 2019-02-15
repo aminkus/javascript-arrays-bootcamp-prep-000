c349076d99aa823139c5fa953d908bd65d46b82f
Adam Brett Minkus <adam.minkus@gmail.com>
2019-02-15 04:54:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,8 @@ function accessElementInArray(array, index){return array[2]}
 function destructivelyRemoveElementFromBeginningOfArray(array){return array.shift()}
 function removeElementFromBeginningOfArray(array){ return array.slice(1)}
 function destructivelyRemoveElementFromEndOfArray(array){return array.pop()}
+function destructivelyRemoveElementFromEndOfArray(array){return array.slice(0)}
+
 
 
 
